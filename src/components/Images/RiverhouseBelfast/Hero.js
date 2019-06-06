755e3916c9033b7c3d990d63912ea35607ded721
Phil Collins <46894268+pcollinsTech@users.noreply.github.com>
2019-06-06 09:43:25 +0100

    Create Hero.js

---
+++ src/components/Images/RiverhouseBelfast/Hero.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const RiverhouseBelfast = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "50009502_2242236916019618_7563559900723281920_o.jpg" }) {
+          childImageSharp {
+            fixed(width: 500) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      return <Img fixed={data.placeholderImage.childImageSharp.fixed} />
+    }}
+  />
+)
+
+export default RiverhouseBelfast
